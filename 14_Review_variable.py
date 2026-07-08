3656c3abdcd6a8eea8bc62cdc9dcc068ad14e601
Phal Developer <sophalsam04@gmail.com>
2026-07-08 22:31:54 +0700

    Create 14_Review_variable.py

---
+++ 14_Review_variable.py
@@ -0,0 +1,49 @@
+# ==============================
+     # Variable / Data type
+#===============================
+id = 101
+name = "un virak"
+age = 22
+
+print("ID :", id);
+print("Name : ", name);
+print("Age :", age)
+
+a = 10;
+sq = 2.3;
+address = "Kandal";
+flag = True
+
+print("================");
+print("A : ", a);
+print("SQ : ", sq);
+print("Address : ", address);
+print("True : ", flag);
+
+print("========Complex Number=========");
+z = 3+4j;
+print("Complex number : ", z);
+
+com = 5+2j;
+print("Complez : ", com);
+
+c = 4+3j;
+print("C : ", c);
+
+p = complex(10,-2);
+print("Complex number : ", p);
+
+
+v = complex(-14,-4);
+print("V : ", v);
+
+a = 3+2j;
+b = 3+5j;
+print("A + B : ", (a+b));
+
+g = complex(4,5);
+h = complex(5,5);
+print("Adding Complex number :",g+h);
+print("Substraction complex number : ", (g-h));
+print("Multiplications complex number : ", (g*h));
+print("Division complex number : ", (g/h));
