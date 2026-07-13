121d42f3c98d151f08a7302cb9af5e2a1023f7bb
Phal Developer <sophalsam04@gmail.com>
2026-07-13 19:48:04 +0700

    Add files via upload

---
+++ 1_package_management.py
@@ -0,0 +1,15 @@
+   # Package 
+        # Install pip install request
+# pip install package	ដំឡើង Package
+# pip uninstall package	លុប Package
+# pip list	បង្ហាញ Package ទាំងអស់
+# pip show package	មើលព័ត៌មាន Package
+# pip freeze	បង្កើតបញ្ជី Package
+# pip install -r requirements.txt	ដំឡើង Package ពី requirements.txt
+# pip install --upgrade package	ធ្វើបច្ចុប្បន្នភាព Package
+# python -m venv myenv	បង្កើត Virtual Environment
+# deactivate	ចាកចេញពី Virtual Environm
+# pip install requests
+# pip install numpy=2.0.0
+# pip install pandas matplotlib
+
---
+++ 2_import_package.py
@@ -0,0 +1,5 @@
+   # Import package
+import requests;
+
+respone = requests.get("https://example.com");
+print(respone.status_code);
